0bf8ad146083da6559643d6fa0cbaafbc6e0be66
pmully94 <peter.mulard@yahoo.com>
2020-07-30 11:55:49 -0700

    header styling

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -5,17 +5,17 @@ export default class Navbar extends React.Component{
     render() {
         return (
             <div class="container-fluid">
-                <div class="row justify-content-between navbar-strip">
+                <div class="row justify-content-between navbar-strip align-items-center">
 			        <div class="logo-box col-auto shrink">
                         <a class="logo" href="#">
-                            <img id="logo-img" src="" class="d-inline-block align-top" alt=""/>
+                            <img id="logo-img" src="logoBlack.png" class="d-inline-block align-top" alt=""/>
                         </a>
 			        </div>
                     <div class="navbar-links-group col-9 col-sm-8 col-lg-8 col-xl-6">
                         <a class="navbar-link" href="#">Photography</a>
                         <a class="navbar-link" href="#">Film</a>
                         <a class="navbar-link" href="#">Sculpture</a>
-                        <a class="navbar-link" href="#">Software</a>
+                        <a class="navbar-link" id="software" href="#">Software</a>
                     </div>
                 </div>
             </div>
