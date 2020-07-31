5f5cac4c9dbd86d7fdec46f98d13bc624c7dd80c
pmully94 <peter.mulard@yahoo.com>
2020-07-31 15:36:14 -0700

    styling

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -8,14 +8,15 @@ export default class Navbar extends React.Component{
                 <div class="row justify-content-between navbar-strip align-items-center">
 			        <div class="logo-box col-auto shrink">
                         <a class="logo" href="https://petermulard.com">
-                            <img id="logo-img" src="logoColor3.png" class="d-inline-block align-top" alt="Peter Mulard" onMouseOver="this.src='logoColor4.png'"/>
+                            <img id="logo-img" src="logoColor2.png" class="d-inline-block align-top" alt="Peter Mulard" onMouseOver="this.src='logoColor4.png'"/>
+                            <img id="logo-img-2" src="logoColor3.png" class="d-inline-block align-top" alt="Peter Mulard" onMouseOver="this.src='logoColor4.png'"/>
                         </a>
 			        </div>
                     <div class="navbar-links-group col-8 col-sm-8 col-lg-8 col-xl-8">
-                        <a class="navbar-link" href="#">Photography</a>
-                        <a class="navbar-link" href="#">Film</a>
-                        <a class="navbar-link" href="#">Sculpture</a>
-                        <a class="navbar-link" id="software" href="#">Software</a>
+                        <a class="navbar-link" href="https://petermulard.com">Photography</a>
+                        <a class="navbar-link" href="https://petermulard.com/film">Film</a>
+                        <a class="navbar-link" href="https://petermulard.com/sculpture">Sculpture</a>
+                        <a class="navbar-link" id="software" href="https://petermulard.me">Software</a>
                     </div>
                 </div>
             </div>
