528085e2440df3bf07c57c649d0dee4a1c3820cd
pmully94 <peter.mulard@yahoo.com>
2020-08-04 17:21:26 -0700

    configure and connect mongoDB to server

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Configuration
+const db = 'mongodb://localhost:27017/photography_website';
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log('Database Error'));
+
+// Sets local port
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
 
 app.get('/api/photoInfo', (req, res) => {
     // Get from database but we will hardcode for now
@@ -13,6 +34,3 @@ app.get('/api/photoInfo', (req, res) => {
     res.json(photoInfo);
 })
 
-const port = 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
