66414431f90b111f98aff92ebb407739f7d0d370
pmully94 <peter.mulard@yahoo.com>
2020-08-05 10:52:05 -0700

    fixed routing bug

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Configuration
+// ** Need to fix bug and hook up with config/keys.js **
 const db = 'mongodb://localhost:27017/photography_website';
 
 // Connect to MongoDB
@@ -24,10 +25,12 @@ app.use('./routes/api/photoItems', photoItems);
 // Sets local port
 const port = 5000;
 
+// Starts server on local port
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 
 
+// GET from db and serve @ localhost:5000/api/photoInfo
 app.get('/api/photoInfo', (req, res) => {
     // Get from database but we will hardcode for now
     const photoInfo = [];
