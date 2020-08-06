a06bf58067af7360e2d741b6d178e03cdc9be9d3
pmully94 <peter.mulard@yahoo.com>
2020-08-05 19:28:16 -0700

    db POST api

--- routes/api/photoItems.js
+++ routes/api/photoItems.js
@@ -13,4 +13,21 @@ router.get('/', (req, res) => {
     .then(items => res.json(items))
 });
 
+// @route   POST api/photoItems
+// @desc    Add new photo item to database
+// @access  Public
+router.post('/', (req, res) => {
+    const newPhotoItem = new PhotoItem({
+        name: req.body.name,
+        url: req.body.url,
+        location: req.body.location,
+        sizes: req.body.sizes,
+        prices: req.body.prices,
+        tags: req.body.tags,
+        rating: req.body.rating
+    })
+
+    newPhotoItem.save().then(photoItem => res.json(photoItem));
+});
+
 module.exports = router;
