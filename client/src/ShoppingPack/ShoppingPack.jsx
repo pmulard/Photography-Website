41435cb93055ab7da0876ff97edde51afdfa144d
pmully94 <peter.mulard@yahoo.com>
2020-08-07 22:17:49 -0700

    layout of pack tiles

--- client/src/ShoppingPack/ShoppingPack.jsx
+++ client/src/ShoppingPack/ShoppingPack.jsx
@@ -19,11 +19,105 @@ export default class ShoppingPack extends React.Component {
                     <NavBar/>
                 </div>
                 <div class="row justify-content-center align-items-center">
-                    <div class="col-11 col-sm-10 col-md-9 col-lg-5 col-xl-5" id="pack-header-container">
-                        <div id="pack-header">Your pack is empty. Let's fill it up!</div>
+                    <div class="col-11 col-sm-10 col-md-9 col-lg-7 col-xl-7" id="pack-header-container">
+                        <div class="pack-header">Your pack is empty. Let's fill it up!</div>
                         <div id="pack-description">Hover over photos to select sizing options.</div>
                         <div id="continue-shopping-button">
                             <a href="/">Continue Shopping</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-11 col-sm-10 col-md-9 col-lg-7 col-xl-7" id="pack-container">
+                        <div class="pack-header">Pack:</div>
+                        <div id="photo-item-container">
+                            <div class="photo-item row">
+                                <div class="photo-item-img-container col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                                    <img src="/photographyPhotos/Fossile_Lake.jpg" alt=""/>
+                                </div>
+                                <div class="photo-item-info-container container-fluid col-8 col-sm-9 col-md-9 col-lg-9 col-xl-9 ">
+                                    <div class="photo-item-info-box row"> 
+                                        <div class="size col-3">
+                                            <div class="photo-item-info-label">Size:</div>
+                                            <div class="photo-item-info-value">16'x24'</div>
+                                        </div>
+                                        <div class="item-price col-3">
+                                            <div class="photo-item-info-label">Price:</div>
+                                            <div class="photo-item-info-value">$24.00</div>
+                                        </div>
+                                        <div class="quantity-container col-3">
+                                            <div class="photo-item-info-label">Count:</div>
+                                            <div class="photo-item-info-value">2</div>
+                                            <div class="photo-item-quantity-buttons-container">
+                                                <span class="quantity-decrease">&nbsp;&nbsp;-&nbsp;</span>
+                                                <span class="quantity-increase">+</span>
+                                            </div>
+                                        </div>
+                                        <div class="total-price col-3">
+                                            <div class="photo-item-info-label">Total:</div>
+                                            <div class="photo-item-info-value">$48.00</div>
+                                        </div>
+                                    </div>   
+                                </div>
+                            </div>
+                            <div class="photo-item row">
+                                <div class="photo-item-img-container col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                                    <img src="/photographyPhotos/Onion_Valley_MilkyWay.jpg" alt=""/>
+                                </div>
+                                <div class="photo-item-info-container col-8 col-sm-9 col-md-9 col-lg-9 col-xl-9 ">
+                                    <div class="photo-item-info-box row"> 
+                                        <div class="size col-3">
+                                            <div class="photo-item-info-label">Size:</div>
+                                            <div class="photo-item-info-value">16'x24'</div>
+                                        </div>
+                                        <div class="item-price col-3">
+                                            <div class="photo-item-info-label">Price:</div>
+                                            <div class="photo-item-info-value">$24.00</div>
+                                        </div>
+                                        <div class="quantity-container col-3">
+                                            <div class="photo-item-info-label">Count:</div>
+                                            <div class="photo-item-info-value">2</div>
+                                            <div class="photo-item-quantity-buttons-container">
+                                                <span class="quantity-decrease">&nbsp;&nbsp;-&nbsp;</span>
+                                                <span class="quantity-increase">+</span>
+                                            </div>
+                                        </div>
+                                        <div class="total-price col-3">
+                                            <div class="photo-item-info-label">Total:</div>
+                                            <div class="photo-item-info-value">$48.00</div>
+                                        </div>
+                                    </div>   
+                                </div>
+                            </div>
+                            <div class="photo-item row">
+                                <div class="photo-item-img-container col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                                    <img src="/photographyPhotos/Fish_Bowl_Sunset.jpg" alt=""/>
+                                </div>
+                                <div class="photo-item-info-container col-8 col-sm-9 col-md-9 col-lg-9 col-xl-9 ">
+                                    <div class="photo-item-info-box row"> 
+                                        <div class="size col-3">
+                                            <div class="photo-item-info-label">Size:</div>
+                                            <div class="photo-item-info-value">16'x24'</div>
+                                        </div>
+                                        <div class="item-price col-3">
+                                            <div class="photo-item-info-label">Price:</div>
+                                            <div class="photo-item-info-value">$24.00</div>
+                                        </div>
+                                        <div class="quantity-container col-3">
+                                            <div class="photo-item-info-label">Count:</div>
+                                            <div class="photo-item-info-value">2</div>
+                                            <div class="photo-item-quantity-buttons-container">
+                                                <span class="quantity-decrease">&nbsp;&nbsp;-&nbsp;</span>
+                                                <span class="quantity-increase">+</span>
+                                            </div>
+                                        </div>
+                                        <div class="total-price col-3">
+                                            <div class="photo-item-info-label">Total:</div>
+                                            <div class="photo-item-info-value">$48.00</div>
+                                        </div>
+                                    </div>   
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
