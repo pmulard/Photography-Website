f327b8a6bf3e7b466aac50f20e682735b939b47d
pmully94 <peter.mulard@yahoo.com>
2020-08-09 00:38:32 -0700

    react-redux; redux-thunk; import Provider

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,26 @@ import Film from './Film/Film';
 import ShoppingPack from './ShoppingPack/ShoppingPack';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { Provider } from 'react-redux';
+import createStore from './createReduxStore'
+
+const store = createStore()
+
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact component={() => <Photography />} />
-          <Route path="/film" exact component={() => <Film />} />
-          {/* <Route path="/sculpture" exact component={() => <Sculpture />} /> */}
-          <Route path="/pack" exact component={() => <ShoppingPack />} />
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/" exact component={() => <Photography />} />
+            <Route path="/film" exact component={() => <Film />} />
+            {/* <Route path="/sculpture" exact component={() => <Sculpture />} /> */}
+            <Route path="/pack" exact component={() => <ShoppingPack />} />
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
