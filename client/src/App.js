eebaaaa5b14c98a559d2d04da95abcdfa09bcb82
pmully94 <peter.mulard@yahoo.com>
2020-08-09 13:47:32 -0700

    move store to store.js; move store.js to /src

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
+// React dependencies
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Components and Routing
 import Photography from './Photography/Photography';
 import Film from './Film/Film';
 // import Sculpture from './Sculpture/Sculpture';
 import ShoppingPack from './ShoppingPack/ShoppingPack';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Redux components
 import { Provider } from 'react-redux';
 import store from './store';
-
-const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
