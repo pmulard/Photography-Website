9d0902681795825d9b5af22c2a91377b6be7d666
pmully94 <peter.mulard@yahoo.com>
2020-08-09 20:35:27 -0700

    inititalize packItems server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const photoItems = require('./routes/api/photoItems');
+const packItems = require('./routes/api/packItems');
 
 const app = express();
 
@@ -21,6 +22,7 @@ mongoose
 
 //Use Routes
 app.use('./routes/api/photoItems', photoItems);
+app.use('./routes/api/packItems', packItems);
 
 // Sets local port
 const port = 5000;
@@ -31,14 +33,35 @@ app.listen(port, () => console.log(`Server started on port ${port}`));
 
 
 // GET from db and serve @ localhost:5000/api/photoInfo
-app.get('/api/photoInfo', (req, res) => {
+app.get('/api/photoItems', (req, res) => {
     // Get from database but we will hardcode for now
     const photoInfo = [
-        {id: '111', fileName: 'WindAndSea_Beach.jpg', prints: {print2x3: 20.00, print4x6: 26.00}},
-        {id: '222', fileName: 'Canyonlands_MilkyWay.jpg', prints: {print5x7: 30.00, print10x14: 38.00}},
-        {id: '333', fileName: 'Genie_Lamp_Rock.jpg', prints: {print2x3: 20.00, print4x6: 26.00}}
+        {id: '111', url: 'WindAndSea_Beach.jpg', prints: {print2x3: 20.00, print4x6: 26.00}},
+        {id: '222', url: 'Canyonlands_MilkyWay.jpg', prints: {print5x7: 30.00, print10x14: 38.00}},
+        {id: '333', url: 'Genie_Lamp_Rock.jpg', prints: {print2x3: 20.00, print4x6: 26.00}}
     ];
 
     res.json(photoInfo);
+})
+
+app.get('/api/packItems', (req, res) => {
+    const packItems = [
+        {
+            id: '444',
+            url: 'Glen_Pass_MilkyWay.jpg',
+            size: '16x24',
+            price: 28.00,
+            quantity: 2
+        },
+        {
+            id: '555',
+            url: 'Fossile_Lake.jpg',
+            size: '8x12',
+            price: 16.00,
+            quantity: 1
+        }
+    ];
+
+    res.json(packItems);
 })
 
