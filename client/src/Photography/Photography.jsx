d57cbb96bcca7dabdd7e7c58fdee9ab70488b123
pmully94 <peter.mulard@yahoo.com>
2020-08-10 17:12:44 -0700

    Configures modal and click functionality

--- client/src/Photography/Photography.jsx
+++ client/src/Photography/Photography.jsx
@@ -1,22 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Photography.css';
 import NavBar from '../NavBar/NavBar';
+import { Modal, Button }from 'react-bootstrap';
+
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+}
 
 export default class Photography extends React.Component {
+    
+    // const [modalShow, setModalShow] = React.useState(false);
+    constructor(props) {
+        super(props);
+        this.state = {
+          modalShow: true,
+          setModalShow: false
+        }
+    }
 
+    modalShow = () => {
+        this.setState({modalShow: true});
+    } 
+
+    modalHide = () => {
+        this.setState({modalShow: false});
+    }
+    
     render() {
         return (
             <div class="container-fluid" id="max-container">
                 <div class="row justify-content-center align-items-center" id="navbar-row">
                     <NavBar/>
                 </div>
+                <MyVerticallyCenteredModal
+                    show={this.state.modalShow}
+                    onHide={this.modalHide}
+                />
                 <div class="row justify-content-center align-items-center">
                     <div class="image-container col-10 col-md-8 col-lg-6">
                         <div id="image-location">California</div>
-                        <a href="/pack"><div id="main-image-box">
+                        <Button variant="primary" onClick={this.modalShow} id="ADD-PACK-BUTTON"><div id="main-image-box">
                             <img src="photographyPhotos/Sky_Top_Lakes.jpg" alt="" class="img-fluid mx-auto d-block" id="main-image"/>
                             <div id="add-pack-button" class="container-fluid"><img src="/icons/plusSign.svg" alt=""/></div>
-                        </div></a>
+                        </div></Button>
                     </div>
                 </div>
                 <div class="row justify-content-center align-items-center">
@@ -51,7 +101,6 @@ export default class Photography extends React.Component {
                         <div class="card"><img src="photographyPhotos/Kearsarge_Lakes_Sunrise.jpg" alt=""/></div>
                     </div>
                 </div>
-
             </div>
         );
     }
