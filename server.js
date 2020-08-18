e097ec750fce237ba8ae84e3dc2659ff5aac6f70
pmully94 <peter.mulard@yahoo.com>
2020-08-17 19:08:51 -0700

    newURLparser and unifiedTopology for mongoose connection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = require('./config/keys').mongoAtlasURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db)
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log('Database Error'));
 
