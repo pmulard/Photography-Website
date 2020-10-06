e790770821ed1b30cb5258d495b6e28c69669e68
Peter Mulard <peter@Peters-MacBook-Pro.local>
2020-10-06 15:58:25 -0700

    remove local mongoDB key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/photography_website',
     mongoAtlasURI: 'mongodb+srv://pmulard:2i1UtGXCUPkuCeMg@photography-website.gzydu.mongodb.net/photography_website?retryWrites=true&w=majority'
 }
