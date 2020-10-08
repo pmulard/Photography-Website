37fb82e948af1a7a871b90d41311a551ffa1b7d7
Peter Mulard <peter.mulard@gmail.com>
2020-10-07 18:01:38 -0700

    updated atlas key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoAtlasURI: 'mongodb+srv://pmulard:2i1UtGXCUPkuCeMg@photography-website.gzydu.mongodb.net/photography_website?retryWrites=true&w=majority'
+    mongoAtlasURI: 'mongodb+srv://pmulard:HxaT48bBBEfmDShm@photography-website.gzydu.mongodb.net/photography_website?retryWrites=true&w=majority'
 }
